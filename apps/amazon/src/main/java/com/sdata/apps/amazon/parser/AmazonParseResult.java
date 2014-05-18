99dd894c94ae4766f91dd6b27ad9d34ff826bace
houdejun214 <houdejun214@gmail.com>
2014-05-18 14:49:10 +0800

    add amazon crawler

---
+++ apps/amazon/src/main/java/com/sdata/apps/amazon/parser/AmazonParseResult.java
@@ -0,0 +1,22 @@
+package com.sdata.apps.amazon.parser;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.sdata.core.parser.ParseResult;
+
+public class AmazonParseResult extends ParseResult {
+	
+	private List<Map<String,Object>> newCategoryList = new ArrayList<Map<String,Object>>();
+
+	public List<Map<String, Object>> getNewCategoryList() {
+		return newCategoryList;
+	}
+
+	public void setNewCategoryList(List<Map<String, Object>> newCategoryList) {
+		this.newCategoryList = newCategoryList;
+	}
+	
+}
